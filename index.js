6b5ec5927a36da33768b368f646327f343cd60a0
Veli-Matti Heino <vekkis@gmail.com.com>
2023-09-18 16:39:16 +0300

    videochat

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors());
 
 const general = [];
 const random = [];
+const rooms = {};
 
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
@@ -53,6 +54,33 @@ io.on('connection', (socket) => {
     io.to('random').emit('chat message', random);
 
   });
+
+  socket.on("join videochat room", (roomID) => {
+    console.log('joined videochat room: ', roomID);
+    if (rooms[roomID]) {
+        rooms[roomID].push(socket.id);
+    } else {
+        rooms[roomID] = [socket.id];
+    }
+    const otherUser = rooms[roomID].find(id => id !== socket.id);
+    if (otherUser) {
+        socket.emit("other user", otherUser);
+        socket.to(otherUser).emit("user joined", socket.id);
+    }
+});
+
+socket.on("offer", payload => {
+    io.to(payload.target).emit("offer", payload);
+});
+
+socket.on("answer", payload => {
+    io.to(payload.target).emit("answer", payload);
+});
+
+socket.on("ice-candidate", incoming => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+});
+
 });
 
 http.listen(3000, () => {
